e1a6764bd25eb1fca5f0bbe86803220fb4255d4b
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-16 16:57:39 -0300

    feat(service-ticket): injeta as dependencias no modulo para usar o schema do DB

--- service-ticket/src/app.module.ts
+++ service-ticket/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TicketModule } from './ticket/ticket.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TicketModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(String(process.env.MONGODB_URL)),
+    TicketModule,
+  ],
   controllers: [],
 })
 export class AppModule {}
--- service-ticket/src/ticket/ticket.module.ts
+++ service-ticket/src/ticket/ticket.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TicketSchema } from './schemas/ticket.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Ticket', schema: TicketSchema }]),
+  ],
   providers: [TicketService],
-  controllers: [TicketController]
+  controllers: [TicketController],
 })
 export class TicketModule {}
