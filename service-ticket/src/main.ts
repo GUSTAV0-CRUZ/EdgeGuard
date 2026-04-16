ee69969e092d8609af6e0f1a285f3b2efe763fde
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-15 23:33:30 -0300

    feat(service-ticket): adciona a estrutura inicial

---
+++ service-ticket/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [String(process.env.RABBITMQ_URL)],
+        queue: 'service-ticket',
+        noAck: false,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
