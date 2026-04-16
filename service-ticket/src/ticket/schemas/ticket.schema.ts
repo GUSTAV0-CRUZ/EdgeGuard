e20ae450732945c34cc26817067ea500c26b8865
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-16 16:49:20 -0300

    feat(service-ticket): cria o schema do ticket

---
+++ service-ticket/src/ticket/schemas/ticket.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Ticket } from '../entities/ticket.entitie';
+import { StatusTicket } from '../enums/status-ticket.enum';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class TicketSchemaDb implements Ticket {
+  @Prop({ type: Number, required: true })
+  number!: number;
+
+  @Prop({ enum: StatusTicket, type: String, default: StatusTicket.PENDING })
+  status!: StatusTicket;
+}
+
+export type TicketSchemaDocument = Document & TicketSchemaDb;
+
+export const TicketSchema = SchemaFactory.createForClass(TicketSchemaDb);
