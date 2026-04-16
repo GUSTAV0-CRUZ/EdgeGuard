68046514b4b5042ecc59633d93272c8ab353d36d
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-16 19:15:56 -0300

    feat(service-ticket): adiciona os endpoints do servico

--- service-ticket/src/ticket/ticket.controller.ts
+++ service-ticket/src/ticket/ticket.controller.ts
@@ -1,4 +1,103 @@
 import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { Channel, Message } from 'amqplib';
+import { TicketService } from './ticket.service';
+import { CreateTicketDto } from './dtos/create-ticket.dto';
 
 @Controller('ticket')
-export class TicketController {}
+export class TicketController {
+  constructor(private readonly ticketService: TicketService) {}
+
+  @EventPattern('create-ticket')
+  async create(
+    @Payload() createTicketDto: CreateTicketDto,
+    @Ctx() ctx: RmqContext,
+  ) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.create(createTicketDto);
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+
+  @MessagePattern('findAll-ticket')
+  async findAll(@Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.findAll();
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+
+  @MessagePattern('findOne-ticket')
+  async findOne(@Payload() id: string, @Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.findOne(id);
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+
+  @EventPattern('delete-ticket')
+  async delete(@Payload() id: string, @Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.delete(id);
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+
+  @EventPattern('reserve-ticket')
+  async reserve(@Payload() id: string, @Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.reserve(id);
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+
+  @EventPattern('cancelReserve-ticket')
+  async cancelReserve(@Payload() id: string, @Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef() as Channel;
+    const originalMsg = ctx.getMessage() as Message;
+
+    try {
+      await this.ticketService.cancelReserve(id);
+      channel.ack(originalMsg);
+    } catch (error: any) {
+      channel.ack(originalMsg);
+      throw error;
+    }
+  }
+}
--- service-ticket/src/ticket/ticket.service.ts
+++ service-ticket/src/ticket/ticket.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTicketDto } from './dtos/create-ticket.dto';
+import { UpdateTicketDto } from './dtos/update-ticket.dto';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+  async create(createTicketDto: CreateTicketDto) {}
+
+  async findAll() {}
+
+  async findOne(id: string) {}
+
+  private async update(id: string, updateTicketDto: UpdateTicketDto) {}
+
+  async delete(id: string) {}
+
+  async reserve(id: string) {}
+
+  async cancelReserve(id: string) {}
+}
