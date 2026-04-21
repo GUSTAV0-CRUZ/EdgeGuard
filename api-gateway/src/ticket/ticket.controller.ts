f191d96a14b53b046302819d7fa2eb056a9009cc
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-21 19:44:06 -0300

    feat(api-gateway): adiciona a busca de um ticket por id

--- api-gateway/src/ticket/ticket.controller.ts
+++ api-gateway/src/ticket/ticket.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { CreateTicketDto } from './dtos/create-ticket.dto';
 import { ClientProxyRmqService } from '../client-proxy-rmq/client-proxy-rmq.service';
 import { ClientProxy } from '@nestjs/microservices';
@@ -28,19 +28,14 @@ export class TicketController {
     return this.serviceTicketclientProxy.send('findAll-ticket', '');
   }
 
-  // @MessagePattern('findOne-ticket')
-  // async findOne(@Payload() id: string, @Ctx() ctx: RmqContext) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     const ticket = await this.ticketService.findOne(id);
-  //     channel.ack(originalMsg);
-  //     return ticket;
-  //   } catch (error: any) {
-  //     catchResilienceMessage(error, channel, originalMsg);
-  //   }
-  // }
+  @ApiResponse({
+    status: 200,
+    type: ResponseTicketDto,
+  })
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.serviceTicketclientProxy.send('findOne-ticket', id);
+  }
 
   // @EventPattern('delete-ticket')
   // async delete(@Payload() id: string, @Ctx() ctx: RmqContext) {
