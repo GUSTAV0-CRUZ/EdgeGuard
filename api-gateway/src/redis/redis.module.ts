1ec4ad9ef927a46a50f33665d78650a76129e8f0
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-27 11:47:32 -0300

    fix(api-gateway) corrige conexao do redis no cacheModule, antes estava usando o localhost e agora usa a mesma que e aberta no RedisClient

--- api-gateway/src/redis/redis.module.ts
+++ api-gateway/src/redis/redis.module.ts
@@ -1,30 +1,27 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
-import { ConfigService } from '@nestjs/config';
 import {
   RedisClient,
   RedisClientProvider,
 } from './providers/redis-client.provider';
+import KeyvRedis, { Keyv } from '@keyv/redis';
 
 @Global()
 @Module({
   imports: [
     CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: async (
-        configService: ConfigService,
-        redisClient: RedisClient,
-      ) => ({
+      // eslint-disable-next-line @typescript-eslint/require-await
+      useFactory: async (redisClient: RedisClient) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        store: await redisStore({
-          redis: redisClient,
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          ttl: configService.get('REDIS_TTL'),
-        } as any),
-      }),
-      inject: [ConfigService, RedisClient],
+        const keyvRedis = new KeyvRedis(redisClient as any);
+
+        return {
+          store: new Keyv({ store: keyvRedis }),
+        };
+      },
+      inject: [RedisClient],
     }),
   ],
   providers: [RedisService, RedisClientProvider],
